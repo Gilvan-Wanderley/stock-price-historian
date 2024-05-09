6d0aa212285ef4d3bb4c895e619c30933c025a2b
Gilvan Wanderley <gilvan.wanderley@outlook.com>
2024-05-09 14:13:59 -0300

    Version for test deploy

--- function_app.py
+++ function_app.py
@@ -1,12 +1,18 @@
 import logging
+import os
 import azure.functions as func
+from supabase import create_client, Client
+
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 30 23 * * *", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="0 */1 * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def trigger_24h(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('Python timer trigger function executed.')
+    url = os.environ["URL"]
+    key = os.environ["KEY"]
+    client: Client = create_client(url, key)
+    client.table("Test").insert({}).execute()
