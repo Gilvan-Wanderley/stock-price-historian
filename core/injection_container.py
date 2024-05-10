a69b9a3e5b192fb93626332ee6e936cfe38c31f8
Gilvan Wanderley <gilvan.wanderley@outlook.com>
2024-05-10 17:51:42 -0300

    Refactor injection container

--- core/injection_container.py
+++ core/injection_container.py
@@ -3,7 +3,10 @@ from supabase import create_client
 from .service import FinanceService, SupabaseService
 
 def database():
-    return SupabaseService(create_client(os.environ["URL"], os.environ["KEY"]))
+    return SupabaseService(create_client(os.environ["URL"], 
+                                         os.environ["KEY"],
+                                         os.environ["EMAIL"],
+                                         os.environ["PASSWORD"]))
 
 def finance():
     return FinanceService(yfinance.Ticker)
