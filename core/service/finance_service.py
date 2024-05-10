3fb660ed6db1a2626af7d2879bc89448d4278039
Gilvan Wanderley <gilvan.wanderley@outlook.com>
2024-05-09 23:22:22 -0300

    Update requirements

---
+++ core/service/finance_service.py
@@ -0,0 +1,6 @@
+import yfinance as yf
+import pandas as pd
+from datetime import datetime
+
+class FinanceService:
+    pass
---
+++ core/service/supabase_service.py
@@ -0,0 +1,5 @@
+from datetime import datetime
+from supabase import create_client, Client
+
+class SupabaseService:
+    pass
